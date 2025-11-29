a138a372c04f12127aaf705689530707ea6e3996
Daniel Bogdan <89947583+DanielBogdan113213@users.noreply.github.com>
2025-11-29 20:32:07 -0300

    cosas para que funcione en cualquier maquina, docker no anda
    docker no anda

--- src/main/java/com/mylogisticcba/common/rateLimit/RateLimitCleanUpJob.java
+++ src/main/java/com/mylogisticcba/common/rateLimit/RateLimitCleanUpJob.java
@@ -1,18 +1,22 @@
 package com.mylogisticcba.common.rateLimit;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.Instant;
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class RateLimitCleanUpJob {
-    RateLimitAttemptRepository repository;
+    private final RateLimitAttemptRepository repository;
     /**
      * Limpia registros expirados
      */
     @Scheduled(fixedDelay = 300000) // cada 5 minutos
+    @Transactional
     public void cleanupExpiredAttempts() {
         Instant now = Instant.now();
         repository.deleteExpired(now);
